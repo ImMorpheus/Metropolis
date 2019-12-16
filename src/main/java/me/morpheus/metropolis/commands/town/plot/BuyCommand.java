47817d694f88018ea632d1d3ee30387bfccd3fc7
ImMorpheus <ImMorpheus@users.noreply.github.com>
2019-12-16 23:10:21 +0100

    Add missing economy integration to BuyCommand

--- src/main/java/me/morpheus/metropolis/commands/town/plot/BuyCommand.java
+++ src/main/java/me/morpheus/metropolis/commands/town/plot/BuyCommand.java
@@ -5,15 +5,21 @@ import me.morpheus.metropolis.api.data.plot.PlotKeys;
 import me.morpheus.metropolis.api.data.citizen.CitizenData;
 import me.morpheus.metropolis.api.command.AbstractHomeTownCommand;
 import me.morpheus.metropolis.api.town.Town;
+import me.morpheus.metropolis.util.EconomyUtil;
 import me.morpheus.metropolis.util.TextUtil;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.service.economy.EconomyService;
+import org.spongepowered.api.service.economy.account.Account;
+import org.spongepowered.api.service.economy.transaction.ResultType;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 class BuyCommand extends AbstractHomeTownCommand {
@@ -24,7 +30,27 @@ class BuyCommand extends AbstractHomeTownCommand {
             source.sendMessage(TextUtil.watermark(TextColors.RED, "This plot is not for sale"));
             return CommandResult.empty();
         }
+        final Optional<Account> bOpt = t.getBank();
+        if (!bOpt.isPresent()) {
+            source.sendMessage(TextUtil.watermark(TextColors.RED, "Unable to retrieve Town bank"));
+            return CommandResult.empty();
+        }
+        final EconomyService es = Sponge.getServiceManager().provideUnchecked(EconomyService.class);
 
+        final Optional<Account> accOpt = es.getOrCreateAccount(source.getIdentifier());
+        if (!accOpt.isPresent()) {
+            source.sendMessage(TextUtil.watermark(TextColors.RED, "Unable to retrieve player account"));
+            return CommandResult.empty();
+        }
+        final ResultType result = EconomyUtil.transfer(accOpt.get(), bOpt.get(), es.getDefaultCurrency(), BigDecimal.valueOf(pd.price().get()));
+        if (result == ResultType.ACCOUNT_NO_FUNDS) {
+            source.sendMessage(TextUtil.watermark(TextColors.RED, "Not enough money"));
+            return CommandResult.empty();
+        }
+        if (result != ResultType.SUCCESS) {
+            source.sendMessage(TextUtil.watermark(TextColors.RED, "Error while paying: ", result.name()));
+            return CommandResult.empty();
+        }
         pd.set(PlotKeys.OWNER, Optional.of(source.getUniqueId()));
         source.sendMessage(TextUtil.watermark(TextColors.AQUA, "You bought this plot for ", pd.price().get()));
         pd.set(PlotKeys.PRICE, 0.0);
