dc455222ea025ce77d25846f93465395bdae54ff
Rysaen <francesco.costantino@outlook.com>
2020-03-15 19:41:18 +0100

    Various fixes to entity spawn event
    * Renamed and updated internal handler
    * Updated references

--- src/main/java/me/morpheus/metropolis/plot/listeners/InternalSpawnEntityHandler.java
+++ src/main/java/me/morpheus/metropolis/plot/listeners/InternalSpawnEntityHandler.java
@@ -4,22 +4,26 @@ import me.morpheus.metropolis.api.event.entity.SpawnEntityTownEvent;
 import me.morpheus.metropolis.api.plot.PlotService;
 import me.morpheus.metropolis.event.entity.MPSpawnEntityTownEvent;
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.entity.living.monster.Monster;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.entity.SpawnEntityEvent;
 
-public final class InternalMobSpawnHandler {
+public final class InternalSpawnEntityHandler {
 
     private final PlotService ps;
 
-    public InternalMobSpawnHandler(PlotService ps) {
+    public InternalSpawnEntityHandler(PlotService ps) {
         this.ps = ps;
     }
 
     @Listener(order = Order.PRE, beforeModifications = true)
-    public void onMobSpawn(SpawnEntityEvent event) {
-        if (event.getEntities().stream().anyMatch(entity -> entity instanceof Monster && this.ps.get(entity.getLocation()).isPresent())) {
+    public void onSpawnEntityInit(SpawnEntityEvent event) {
+        event.getEntitySnapshots();
+    }
+
+    @Listener(order = Order.FIRST, beforeModifications = true)
+    public void onSpawnEntity(SpawnEntityEvent event) {
+        if (event.getEntities().stream().anyMatch(entity -> this.ps.get(entity.getLocation()).isPresent())) {
             SpawnEntityTownEvent townEvent = new MPSpawnEntityTownEvent(event.getCause(), event.getEntities(), event.getEntitySnapshots());
             if (Sponge.getEventManager().post(townEvent)) {
                 event.setCancelled(true);
