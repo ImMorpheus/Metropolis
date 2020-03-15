4c3910cbd67ffef531336c26ab07ce1434a24678
Rysaen <francesco.costantino@outlook.com>
2020-03-15 17:39:51 +0100

    Improved entity spawn event

--- src/main/java/me/morpheus/metropolis/api/event/entity/MobSpawnTownEvent.java
+++
@@ -1,7 +0,0 @@
-package me.morpheus.metropolis.api.event.entity;
-
-import org.spongepowered.api.event.Cancellable;
-import org.spongepowered.api.event.entity.TargetEntityEvent;
-
-public interface MobSpawnTownEvent extends TargetEntityEvent, Cancellable {
-}
--- src/main/java/me/morpheus/metropolis/plot/listeners/InternalMobSpawnHandler.java
+++ src/main/java/me/morpheus/metropolis/plot/listeners/InternalMobSpawnHandler.java
@@ -1,16 +1,13 @@
 package me.morpheus.metropolis.plot.listeners;
 
-import me.morpheus.metropolis.api.data.plot.PlotData;
-import me.morpheus.metropolis.api.event.entity.MobSpawnTownEvent;
+import me.morpheus.metropolis.api.event.entity.SpawnEntityTownEvent;
 import me.morpheus.metropolis.api.plot.PlotService;
-import me.morpheus.metropolis.event.entity.MPMobSpawnTownEvent;
+import me.morpheus.metropolis.event.entity.MPSpawnEntityTownEvent;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.monster.Monster;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.Order;
 import org.spongepowered.api.event.entity.SpawnEntityEvent;
-
-import java.util.Optional;
 
 public final class InternalMobSpawnHandler {
 
@@ -20,22 +17,14 @@ public final class InternalMobSpawnHandler {
         this.ps = ps;
     }
 
-    @Listener(order = Order.FIRST, beforeModifications = true)
+    @Listener(order = Order.PRE, beforeModifications = true)
     public void onMobSpawn(SpawnEntityEvent event) {
-        event.filterEntities(entity -> {
-            if (entity instanceof Monster) {
-                Optional<PlotData> pdOpt = this.ps.get(entity.getLocation());
-                if (pdOpt.isPresent()) {
-                    MobSpawnTownEvent mobSpawnEvent = new MPMobSpawnTownEvent(event.getCause(), entity);
-                    if (Sponge.getEventManager().post(mobSpawnEvent)) {
-                        return false;
-                    } else if (!pdOpt.get().mobSpawn().get()) {
-                        return false;
-                    }
-                }
+        if (event.getEntities().stream().anyMatch(entity -> entity instanceof Monster && this.ps.get(entity.getLocation()).isPresent())) {
+            SpawnEntityTownEvent townEvent = new MPSpawnEntityTownEvent(event.getCause(), event.getEntities(), event.getEntitySnapshots());
+            if (Sponge.getEventManager().post(townEvent)) {
+                event.setCancelled(true);
             }
-            return true;
-        });
+        }
     }
 
 }
