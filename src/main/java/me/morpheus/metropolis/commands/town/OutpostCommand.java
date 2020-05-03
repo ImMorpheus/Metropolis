9efe7aeb13abcfac11303919d01a6ab891f1d82d
ImMorpheus <ImMorpheus@users.noreply.github.com>
2020-05-03 23:00:21 +0200

    Merge outposts and outpost

--- src/main/java/me/morpheus/metropolis/commands/town/OutpostCommand.java
+++ src/main/java/me/morpheus/metropolis/commands/town/OutpostCommand.java
@@ -20,19 +20,23 @@ import org.spongepowered.api.event.CauseStackManager;
 import org.spongepowered.api.event.cause.EventContextKeys;
 import org.spongepowered.api.event.cause.entity.teleport.TeleportTypes;
 import org.spongepowered.api.plugin.PluginContainer;
+import org.spongepowered.api.service.pagination.PaginationList;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.text.serializer.TextSerializers;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 class OutpostCommand extends AbstractCitizenCommand {
 
     OutpostCommand() {
         super(
-                GenericArguments.onlyOne(GenericArguments.text(Text.of("name"), TextSerializers.FORMATTING_CODE, false)),
+                GenericArguments.optional(GenericArguments.text(Text.of("name"), TextSerializers.FORMATTING_CODE, false)),
                 MinimalInputTokenizer.INSTANCE,
                 Metropolis.ID + ".commands.town.outpost",
                 Text.of()
@@ -41,8 +45,6 @@ class OutpostCommand extends AbstractCitizenCommand {
 
     @Override
     public CommandResult process(Player source, CommandContext context, CitizenData cd, Town t) throws CommandException {
-        final Text name = context.requireOne("name");
-
         final Optional<OutpostData> odOpt = t.get(OutpostData.class);
 
         if (!odOpt.isPresent()) {
@@ -50,12 +52,31 @@ class OutpostCommand extends AbstractCitizenCommand {
             return CommandResult.empty();
         }
 
+        final Optional<Text> nameOpt = context.getOne("name");
+
+        if (!nameOpt.isPresent()) {
+            final List<Text> outposts = new ArrayList<>(odOpt.get().outposts().size());
+            for (Map.Entry<String, Location<World>> entry : odOpt.get().outposts().get().entrySet()) {
+                outposts.add(Text.of("[", entry.getKey(), "] ", entry.getValue().getBlockPosition(), " (", entry.getValue().getExtent().getName(), ")"));
+            }
+
+            PaginationList.builder()
+                    .title(Text.of(TextColors.GOLD, "[", TextColors.YELLOW, "Outposts", TextColors.GOLD, "]"))
+                    .contents(Text.of(TextColors.AQUA, Text.joinWith(Text.NEW_LINE, outposts)))
+                    .padding(Text.of(TextColors.GOLD, "-"))
+                    .sendTo(source);
+
+            return CommandResult.success();
+        }
+
+        final Text name = nameOpt.get();
         final Location<World> out = odOpt.get().outposts().get().get(name.toPlain());
 
         if (out == null) {
             source.sendMessage(TextUtil.watermark(TextColors.RED, "There is no outpost named ", name));
             return CommandResult.empty();
         }
+
         final Optional<Entity> vehicleOpt = source.getVehicle();
         if (vehicleOpt.isPresent()) {
             boolean success = source.setVehicle(null);
