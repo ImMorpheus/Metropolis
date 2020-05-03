47ea3cf57563a0e6d7a620cf1ac26c2a5ca88703
ImMorpheus <ImMorpheus@users.noreply.github.com>
2020-05-03 23:01:03 +0200

    Merge Upgrade and Upgrades

--- src/main/java/me/morpheus/metropolis/commands/town/UpgradeCommand.java
+++ src/main/java/me/morpheus/metropolis/commands/town/UpgradeCommand.java
@@ -8,19 +8,27 @@ import me.morpheus.metropolis.api.data.citizen.CitizenData;
 import me.morpheus.metropolis.api.town.Town;
 import me.morpheus.metropolis.api.town.Upgrade;
 import me.morpheus.metropolis.util.TextUtil;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.args.GenericArguments;
 import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.service.pagination.PaginationList;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 class UpgradeCommand extends AbstractCitizenCommand {
 
     UpgradeCommand() {
         super(
-                GenericArguments.onlyOne(MPGenericArguments.catalog(Upgrade.class, Text.of("upgrade"))),
+                GenericArguments.optional(
+                        GenericArguments.onlyOne(MPGenericArguments.catalog(Upgrade.class, Text.of("upgrade")))
+                ),
                 MinimalInputTokenizer.INSTANCE,
                 Metropolis.ID + ".commands.town.upgrade",
                 Text.of()
@@ -29,7 +37,26 @@ class UpgradeCommand extends AbstractCitizenCommand {
 
     @Override
     public CommandResult process(Player source, CommandContext context, CitizenData cd, Town t) throws CommandException {
-        final Upgrade upgrade = context.requireOne("upgrade");
+        final Optional<Upgrade> upgradeOpt = context.getOne("upgrade");
+        if (!upgradeOpt.isPresent()) {
+            final Collection<Text> upgrades = Sponge.getRegistry().getAllOf(Upgrade.class).stream()
+                    .filter(upgrade -> upgrade.getRequiredTownTypes().contains(t.getType()))
+                    .map(upgrade -> Text.of(upgrade.getName()))
+                    .collect(Collectors.toList());
+
+            if (upgrades.isEmpty()) {
+                source.sendMessage(TextUtil.watermark(TextColors.AQUA, "Your town has no upgrade"));
+                return CommandResult.empty();
+            }
+
+            PaginationList.builder()
+                    .title(Text.of(TextColors.GOLD, "[", TextColors.YELLOW, "Upgrades", TextColors.GOLD, "]"))
+                    .contents(Text.of(TextColors.AQUA, Text.joinWith(Text.NEW_LINE, upgrades)))
+                    .padding(Text.of(TextColors.GOLD, "-"))
+                    .sendTo(source);
+            return CommandResult.success();
+        }
+        final Upgrade upgrade = upgradeOpt.get();
         final boolean success = t.upgrade(upgrade);
         if (!success) {
             source.sendMessage(TextUtil.watermark(TextColors.RED, "Upgrade failed"));
